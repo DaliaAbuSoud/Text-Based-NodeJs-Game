54bdd8f66c37c70a7d426dd6f023f4256ad2289e
Dalia <Dalia@Miras-Air.fritz.box>
2020-06-18 17:34:58 +0200

    added extra chalk styling

--- script.js
+++ script.js
@@ -1,56 +1,73 @@
 const readline = require("readline");
 const chalk = require("chalk");
+const chalkAnimation = require("chalk-animation");
+const chalkPipe = require("chalk-pipe");
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
-console.log("Welcome to the Body Mass Index (BMI) calculating tool.");
-console.log(
-    "This tool will help you caluclate you current BMI and compare it to the ideal BMI for your body."
-);
-
 const greetingQuestions = {
-    q: "Please type 'next' to proceed or type 'quit' to terminate.",
+    q: " Please press 'enter' to proceed or press 'esc' anytime to terminate.",
     answer: {
-        next: "Now please give us some information about you",
-        quit: "GoodBye!",
+        next: "\nNow please give us some information about you\n",
+        quit: "\nGoodBye!",
     },
 };
 
+// const stdin = process.openStdin();
+// require("tty").setRawMode(true);
+
+// stdin.on("keypress", function (chunk, key) {
+//     // process.stdout.write("Get Chunk: " + chunk + "\n");
+//     if (key === 27) process.exit();
+// });
+
 function greeting(responses) {
-    rl.question(chalk.magenta(responses.q), (answer) => {
+    rl.question(chalkPipe("magenta.bold")(responses.q), (answer) => {
         if (answer == "quit") {
             console.log(chalk.cyan(greetingQuestions.answer.quit));
             rl.close();
             return;
         } else if (answer == "next") {
-            console.log(chalk.cyan(greetingQuestions.answer.next));
+            console.log(chalk.black.bgCyan(greetingQuestions.answer.next));
             askQuestions(personalData);
         }
     });
 }
 
-greeting(greetingQuestions);
+chalkAnimation.rainbow(
+    "\nWelcome to the Body Mass Index (BMI) calculator!\n",
+    0.2
+);
+setTimeout(() => {
+    // Stop the greeting animation, then write on a new line.
+
+    chalkAnimation.rainbow(
+        "This tool will caluclate your current BMI and compare it to the ideal BMI for your body.\n",
+        0.2
+    );
+    setTimeout(() => {
+        greeting(greetingQuestions);
+    }, 3000);
+}, 3000);
 
 const personalData = {
-    nameQ: "What is your first name?",
-    ageQ: "How old are you?",
-    heightQ: "What is your height in Meters?",
-    weightQ: "What is your weight in KGs?",
+    nameQ: "What is your first name?\n",
+    ageQ: "How old are you?\n",
+    heightQ: "What is your height in Meters? (e.g: 1.75)\n",
+    weightQ: "What is your weight in KGs? (e.g: 65.5)\n",
     submit:
-        " Thanks for entering you data. Please type submit if you wisht to know your BMI!",
+        "\nThanks for entering your data. Now please press enter to see your BMI!\n",
 };
 
 function askQuestions(answer) {
     rl.question(chalk.green(personalData.nameQ), (answer) => {
         if (answer) {
-            console.log(`Hello ${answer}`);
+            console.log(chalk.yellow.italic.bold(`\nHello ${answer}\n`));
             rl.question(chalk.green(personalData.ageQ), (answer) => {
-                if (answer == "quit") {
-                    console.log(chalk.yellow("GoodBye"));
-                    rl.close();
-                } else if (answer === NaN || answer == "") {
+                if (answer === NaN || answer == "") {
                     console.log(
                         chalk.red(
                             "Please Enter a Valid Answer or Type 'Quite to Terminate"
@@ -58,10 +75,7 @@ function askQuestions(answer) {
                     );
                 } else if (answer) {
                     rl.question(chalk.green(personalData.heightQ), (answer) => {
-                        if (answer == "quit") {
-                            console.log(chalk.yellow("GoodBye"));
-                            rl.close();
-                        } else if (Number.isNaN(answer) || answer == "") {
+                        if (Number.isNaN(answer) || answer == "") {
                             console.log(
                                 chalk.red(
                                     "Please Enter a Valid Answer or Type 'Quite to Terminate"
@@ -72,10 +86,7 @@ function askQuestions(answer) {
                             rl.question(
                                 chalk.green(personalData.weightQ),
                                 (answer) => {
-                                    if (answer == "quit") {
-                                        console.log(chalk.yellow("GoodBye"));
-                                        rl.close();
-                                    } else if (answer === NaN || answer == "") {
+                                    if (answer === NaN || answer == "") {
                                         console.log(
                                             chalk.red(
                                                 "Please Enter a Valid Answer or Type 'Quite to Terminate"
@@ -84,28 +95,35 @@ function askQuestions(answer) {
                                     } else if (answer) {
                                         var weightAnswer = answer;
                                         rl.question(
-                                            chalk.green(personalData.submit),
+                                            chalk.magenta.bold(
+                                                personalData.submit
+                                            ),
                                             (answer) => {
-                                                if (answer == "quit") {
+                                                if (answer == "submit")
                                                     console.log(
-                                                        chalk.yellow("GoodBye")
-                                                    );
-                                                    rl.close();
-                                                } else if (answer == "submit")
-                                                    console.log(
-                                                        "The BMI indictaes the following weight status: "
+                                                        chalk.black.bgBlue(
+                                                            "\nThe BMI indictaes the following weight status: \n"
+                                                        )
                                                     );
                                                 console.log(
-                                                    "- Underweight: BMI Below 18.5"
+                                                    chalk.yellow(
+                                                        "- Underweight: BMI Below 18.5"
+                                                    )
                                                 );
                                                 console.log(
-                                                    "- Normal: BMI between 18.5 ~ 24.9"
+                                                    chalk.greenBright(
+                                                        "- Normal: BMI between 18.5 ~ 24.9"
+                                                    )
                                                 );
                                                 console.log(
-                                                    "- Overweight: BMI between 25. ~ 29.9"
+                                                    chalk.blue(
+                                                        "- Overweight: BMI between 25. ~ 29.9"
+                                                    )
                                                 );
                                                 console.log(
-                                                    "- Obese: BMI 30 and above"
+                                                    chalk.red(
+                                                        "- Obese: BMI 30 and above\n"
+                                                    )
                                                 );
 
                                                 BMIcalculator(
@@ -127,5 +145,6 @@ function askQuestions(answer) {
 
 function BMIcalculator(weight, height) {
     let BMI = weight / (height * height);
-    console.log("Your BMI is: ", BMI.toFixed(2));
+
+    chalkAnimation.pulse(`*** Your BMI is: ${BMI.toFixed(2)} ***`);
 }
